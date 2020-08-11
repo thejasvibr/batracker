1336d72dc6d2f6b8730321fdf39c2f28043ae633
thejasvi <thejasvib@gmai.com>
2020-08-11 10:04:05 +0200

    implement basic correspondence matcher

--- batracker/tests/prototyping/correspondence_match_prototyping.py
+++ batracker/tests/prototyping/correspondence_match_prototyping.py
@@ -23,36 +23,6 @@ import scipy.spatial as spatial
 
 from threshold_detector_prototyping import cross_channel_threshold_detector
 
-# %%
-# Load the simulated audio file that was previously made
-
-fs, audio = wavfile.read('simulated_audio/batracker_simple.wav')
-short_audio = audio[:fs,:]
-multi_detections = cross_channel_threshold_detector(short_audio, fs)
-
-# %% 
-# Also need some kind of visualiser to see the output of the all the detections 
-# and diagnose problems.
-
-num_channels = audio.shape[1]
-all_axes = []
-plt.figure(figsize=(10,8))
-ax0 = plt.subplot(num_channels*100  + 11)
-plt.specgram(short_audio[:, 0], Fs=fs, NFFT=256, noverlap=255)
-
-for each in range(1,num_channels):
-    plt.subplot(num_channels*100  + 10+each+1, sharex = ax0)
-    plt.specgram(short_audio[:, each], Fs=fs, NFFT=256, noverlap=255)
-    
-    for every in multi_detections[each]:
-        plt.vlines(every, 0, fs*0.5, linewidth=0.5)
-
-# %% 
-# Matching detections to each other. 
-# The sounds are matched together according to the furthest microphone distances. 
-# eg. if the mics 1 and 4 are about 2 m apart, then we expect all sounds
-# to arrive within :math:`\pm`6 ms of each other from on
-array_geometry = pd.read_csv('simulated_audio/array_geom.csv')
 
 def calc_intermic_distances(array_geom):
     '''
@@ -105,9 +75,134 @@ def largest_mic2mic_distance(array_geom):
     maxval_location = np.argwhere(maxval==mic2mic_distances)[0]
     return maxval_location, maxval
     
-loc, max_dist  = largest_mic2mic_distance(array_geometry)
 
-# %% 
-# Now, find all sounds within +/- max delay distance of that mic pair, and
-# put them into one boundary region for cross-correlation
+def calculate_crosscorr_boundary(mic_pair, max_dist, detections, **kwargs):
+    '''
+    Generates the common audio boundaries which will be used to perform
+    the cross-correlation 
+    
+    This function does not inspect the audio content of the 
+    segment that it outputs across channels. 
+    
+    Parameters
+    ----------
+    mic_pair : list/array-like
+        With 2 entries to indicate the index numbers of the mics with 
+        the largest distance.s
+    max_dist : float
+        Distance in meters
+    detections: list
+        List with tuples.
+    vsound : float, optional
+        Defaults to 338 m/s
+    
+    Returns
+    -------
+    crosscor_boundaries
+    
+    Note
+    ----
+    This function assumes there are an equal number of sounds detected across 
+    all channels. An error will be thrown if there are inequal number of detections.
+    '''
+    vsound = kwargs.get('vsound', 338.0)
+    num_detections = np.array([len(each) for each in detections])
+    if not np.all(num_detections==num_detections[0]):
+        raise ValueError(f'All channels do not have an equal number of detections {num_detections}')
+    
+    time_delay = max_dist/vsound
+
+    crosscor_boundaries = []
+
+    for detection_num, detection_times in enumerate(detections[0]):
+        earliest_start = np.min([ detections[mic_pair[0]][detection_num][0],
+                                  detections[mic_pair[1]][detection_num][0]
+                                 ])
+        latest_stop = np.max([ detections[mic_pair[0]][detection_num][1],
+                               detections[mic_pair[1]][detection_num][1]
+                                 ])
+        crosscor_boundaries.append( (earliest_start-time_delay,
+                                     latest_stop+time_delay)
+                                  )
+    return crosscor_boundaries   
+
+
+def generate_crosscor_boundaries(detections, array_geom, **kwargs):
+    '''Wrapper function that calculates the largest mic-2-mic distance
+    and proceeds to generate the common boundaries for cross-correlation. 
+    
+    See Also
+    --------
+    calculate_crosscor_boundary
+    largest_mic2mic_distance
+    
+    '''
+    mic_pair, max_dist  = largest_mic2mic_distance(array_geom)
+    crosscor_boundary = calculate_crosscorr_boundary(mic_pair, max_dist, 
+                                                     detections, **kwargs)
+    return crosscor_boundary
+
+
+
+if __name__ == '__main__':
+
+    # %%
+    # Load the simulated audio file that was previously made
+    
+    fs, audio = wavfile.read('simulated_audio/batracker_simple.wav')
+    short_audio = audio[:fs,:]
+    multi_detections = cross_channel_threshold_detector(short_audio, fs)
+    
+    # %% 
+    # Also need some kind of visualiser to see the output of the all the detections 
+    # and diagnose problems.
+    
+    num_channels = audio.shape[1]
+    all_axes = []
+    plt.figure(figsize=(10,8))
+    ax0 = plt.subplot(num_channels*100  + 11)
+    plt.specgram(short_audio[:, 0], Fs=fs, NFFT=256, noverlap=255)
+    
+    for each in range(1,num_channels):
+        plt.subplot(num_channels*100  + 10+each+1, sharex = ax0)
+        plt.specgram(short_audio[:, each], Fs=fs, NFFT=256, noverlap=255)
+        
+        for every in multi_detections[each]:
+            plt.vlines(every, 0, fs*0.5, linewidth=0.5)
+    
+    # %% 
+    # Matching detections to each other. 
+    # The sounds are matched together according to the furthest microphone distances. 
+    # eg. if the mics 1 and 4 are about 2 m apart, then we expect all sounds
+    # to arrive within :math:`\pm`6 ms of each other from on
+    array_geometry = pd.read_csv('simulated_audio/array_geom.csv')
+    
+    
+    loc,     max_dist  = largest_mic2mic_distance(array_geometry)
+    
+    # %% 
+    # Now, find all sounds within +/- max delay distance of that mic pair, and
+    # put them into one boundary region for cross-correlation. This defines the 
+    # cross-correlation boundaries
+
+    crosscor_boundaries = generate_crosscor_boundaries(multi_detections,array_geometry)
+    
+    for each in range(1,num_channels):
+        plt.subplot(num_channels*100  + 10+each+1, sharex = ax0)    
+        for every in crosscor_boundaries:
+            plt.vlines(every, 0, fs*0.5,'r', linewidth=0.5, )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- batracker/tests/prototyping/start_to_end_prototype.py
+++ batracker/tests/prototyping/start_to_end_prototype.py
@@ -11,12 +11,15 @@ Start to end prototype example
 import matplotlib.pyplot as plt
 plt.rcParams['agg.path.chunksize']=10000
 import numpy as np 
+import pandas as pd
 import scipy.io.wavfile as wavfile
 from threshold_detector_prototyping import cross_channel_threshold_detector
-
+from correspondence_match_prototyping import generate_crosscor_boundaries
 # %% 
 # Load the simulated audio file 
-fs, audio = wavfile.read('simulated_audio/batracker_simple.wav')
+fs, full_audio = wavfile.read('simulated_audio/batracker_simple.wav')
+audio = full_audio[:fs,:] # keep the whole example short first!
+
 
 # %% 
 # Detect the calls in each channel 
@@ -36,9 +39,20 @@ for i in range(2,5):
 
 
 # %% 
-# Perform correspondence matching and generate the common audio boundaries
+# Perform correspondence matching and generate the common boundaries
+# across channels for cross=correlation. Also, load the mic array geometry 
+# as the max- inter-mic distances are required  for the calculation of max
+# inter-mic delays
 
+array_geom = pd.read_csv('simulated_audio/array_geom.csv')
 
+crosscor_boundaries = generate_crosscor_boundaries(detections,array_geom)
+
+num_channels = audio.shape[1]
+for each in range(1,num_channels):
+        plt.subplot(num_channels*100  + 10+each+1, sharex = ax)    
+        for every in crosscor_boundaries:
+            plt.vlines(every, 0, fs*0.5,'r', linewidth=0.5, )
 
 # %% 
 # Estimate time-difference-of-arrival across different channels and sounds
